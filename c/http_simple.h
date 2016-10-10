73dc7b23442f459c2326c0631fe865e50ee7998c
glzjin <glzjin@zhaojin97.cn>
2016-10-11 00:35:01 +0800

    add http_post

--- c/http_simple.h
+++ c/http_simple.h
@@ -13,4 +13,6 @@ void http_simple_dispose(obfs *self);
 int http_simple_client_encode(obfs *self, char **pencryptdata, int datalength, size_t* capacity);
 int http_simple_client_decode(obfs *self, char **pencryptdata, int datalength, size_t* capacity, int *needsendback);
 
+int http_post_client_encode(obfs *self, char **pencryptdata, int datalength, size_t* capacity);
+
 #endif // _HTTP_SIMPLE_H
